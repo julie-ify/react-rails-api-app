5f8d09e88bbba3d0893d6ad51600ff4cae7ec88b
Juliana Ifionu <julieifionu@gmail.com>
2022-01-10 00:49:58 +0100

    Configure react router and render root and home routes

---
+++ app/javascript/components/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import HelloWorld from './HelloWorld'
+
+class App extends React.Component {
+  render () {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element="Home!" />
+          <Route path="/hello" element={<HelloWorld greeting="Friend" />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  root 'static#index'
+
   namespace :v1, default: {format: 'json'} do
     get 'things', to: 'things#index'
   end
+
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+
+  root 'static#index'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
