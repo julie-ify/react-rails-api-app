2b4f2bc9c6617737f4a3d5fd3236ccfa3758d361
Juliana Ifionu <julieifionu@gmail.com>
2022-01-10 12:30:38 +0100

    Add morre items to API

--- app/controllers/v1/things_controller.rb
+++ app/controllers/v1/things_controller.rb
@@ -4,6 +4,10 @@ class V1::ThingsController < ApplicationController
       {
         :name => 'some-thing',
         :guid => '0415674-a0e8-4748-af68-a5f53a05c453'
+      },
+      {
+        :name => 'some-thing2',
+        :guid => '0415674-a0e8-4748-af68-a5f53a05c453'
       }
     ]}.to_json
   end 
